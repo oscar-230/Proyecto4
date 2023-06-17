8724aefbd09cfd9bf00d268a9b30ebfdb7bc1d2d
oscar-230 <111622297+oscar-230@users.noreply.github.com>
2023-06-16 22:58:08 -0500

    Se crea el controlador de acciones para fondo de pension

---
+++ src/Controller/FondoPensionController.java
@@ -0,0 +1,43 @@
+/**
+ * @author Oscar David Cuaical 
+ * @author 
+ * Grupo: 01
+ * N° Laboratorio: 04
+ * Profesor: Luis Yovany Romo Portilla
+ */
+
+package Controller;
+
+import DAO.FondoPensionDAO;
+import Model.FondoPension;
+import java.util.List;
+
+
+public class FondoPensionController {
+    private FondoPensionDAO fondoPensionDAO;
+
+    public FondoPensionController(FondoPensionDAO fondoPensionDAO) {
+        this.fondoPensionDAO = fondoPensionDAO;
+    }
+
+    public void agregarFondoPension(FondoPension fondoPension) {
+        fondoPensionDAO.agregarFondoPension(fondoPension);
+    }
+
+    public void eliminarFondoPension(FondoPension fondoPension) {
+        fondoPensionDAO.eliminarFondoPension(fondoPension);
+    }
+
+    public List<FondoPension> obtenerFondosPension() {
+        return fondoPensionDAO.obtenerFondosPension();
+    }
+
+    public FondoPension buscarFondoPensionPorCodigo(int codigo) {
+        return fondoPensionDAO.buscarFondoPensionPorCodigo(codigo);
+    }
+
+    public void actualizarFondoPensionPorCodigo(int codigo, FondoPension fondoPension) {
+        fondoPensionDAO.actualizarFondoPensionPorCodigo(codigo, fondoPension);
+    }
+    
+}
