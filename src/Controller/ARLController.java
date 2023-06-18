96a7565985e82d6ca39ba705e0ff54e2e2162ccc
oscar-230 <111622297+oscar-230@users.noreply.github.com>
2023-06-17 20:08:49 -0500

    Se crea el controlador para ARL

---
+++ src/Controller/ARLController.java
@@ -0,0 +1,43 @@
+/**
+ * @author Oscar David Cuaical 
+ * @author 
+ * Grupo: 01
+ * N° Laboratorio: 04
+ * Profesor: Luis Yovany Romo Portilla
+ */
+
+package Controller;
+
+import DAO.ARL_DAO;
+import Model.ARL;
+import java.util.List;
+
+
+public class ARLController {
+    private ARL_DAO arlDAO;
+
+    public ARLController(ARL_DAO arlDAO) {
+        this.arlDAO = arlDAO;
+    }
+
+    public void guardarARL(ARL arl) {
+        arlDAO.guardarARL(arl);
+    }
+
+    public ARL buscarARLPorCodigo(int codigo) {
+        return arlDAO.buscarARLPorCodigo(codigo);
+    }
+
+    public List<ARL> obtenerTodasARL() {
+        return arlDAO.obtenerTodasARL();
+    }
+
+    public void actualizarARL(ARL arl) {
+        arlDAO.actualizarARL(arl);
+    }
+
+    public void eliminarARL(ARL arl) {
+        arlDAO.eliminarARL(arl);
+    }
+    
+}
