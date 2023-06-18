ff17a843b85579fb7000d3f6d0e1af9588ff0915
oscar-230 <111622297+oscar-230@users.noreply.github.com>
2023-06-17 21:32:55 -0500

    se crea el controlador de acciones para concepto devengo

---
+++ src/Controller/ConceptoDevengoController.java
@@ -0,0 +1,42 @@
+/**
+ * @author Oscar David Cuaical 
+ * @author 
+ * Grupo: 01
+ * N° Laboratorio: 04
+ * Profesor: Luis Yovany Romo Portilla
+ */
+
+package Controller;
+
+import DAO.ConceptoDevengoDAO;
+import Model.ConceptoDevengo;
+import java.util.List;
+
+
+public class ConceptoDevengoController {
+    private ConceptoDevengoDAO conceptoDevengoDAO;
+
+    public ConceptoDevengoController(ConceptoDevengoDAO conceptoDevengoDAO) {
+        this.conceptoDevengoDAO = conceptoDevengoDAO;
+    }
+
+    public void guardarConceptoDevengo(ConceptoDevengo conceptoDevengo) {
+        conceptoDevengoDAO.guardarConceptoDevengo(conceptoDevengo);
+    }
+
+    public ConceptoDevengo buscarConceptoDevengoPorCodigo(int codigo) {
+        return conceptoDevengoDAO.buscarConceptoDevengoPorCodigo(codigo);
+    }
+
+    public List<ConceptoDevengo> obtenerTodosConceptosDevengo() {
+        return conceptoDevengoDAO.obtenerTodosConceptosDevengo();
+    }
+
+    public void actualizarConceptoDevengo(ConceptoDevengo conceptoDevengo) {
+        conceptoDevengoDAO.actualizarConceptoDevengo(conceptoDevengo);
+    }
+
+    public void eliminarConceptoDevengo(ConceptoDevengo conceptoDevengo) {
+        conceptoDevengoDAO.eliminarConceptoDevengo(conceptoDevengo);
+    }
+}
